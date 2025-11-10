4457bc9fa4944e37e582e425704b510782384da6
OrlPolinka <porlovskaya7@gmail.com>
2025-11-10 08:32:48 +0300

    New commit

---
+++ lab_5/Lab_5/Lab-05b/Lab-05b.cpp
@@ -0,0 +1,70 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <string>
+
+int wmain(int argc, wchar_t* argv[]) {
+    setlocale(LC_ALL, "ru");
+    if (argc != 4) {
+        std::wcerr << L"Использование: Lab-05b <P1: affinity mask> <P2: priority 1> <P3: priority 2>\n";
+        return 1;
+    }
+
+    DWORD_PTR affinityMask = _wtoi64(argv[1]);
+    DWORD priority1 = _wtoi(argv[2]);
+    DWORD priority2 = _wtoi(argv[3]);
+
+    std::wcout << L"Маска процессоров (P1): " << affinityMask << "\n";
+    std::wcout << L"Приоритет дочернего процесса 1 (P2): " << priority1 << "\n";
+    std::wcout << L"Приоритет дочернего процесса 2 (P3): " << priority2 << "\n";
+
+    // Путь к Lab-05x.exe
+    std::wstring childPath = L"Lab-05x.exe";
+
+    // Структуры запуска
+    STARTUPINFOW si1 = { sizeof(si1) };
+    PROCESS_INFORMATION pi1;
+    STARTUPINFOW si2 = { sizeof(si2) };
+    PROCESS_INFORMATION pi2;
+
+    // Первый процесс
+    if (CreateProcessW(
+        childPath.c_str(), nullptr, nullptr, nullptr, FALSE,
+        CREATE_NEW_CONSOLE, nullptr, nullptr, &si1, &pi1)) {
+
+        SetProcessAffinityMask(pi1.hProcess, affinityMask);
+        SetPriorityClass(pi1.hProcess, priority1);
+        std::wcout << L"Процесс 1 запущен: PID = " << pi1.dwProcessId << "\n";
+    }
+    else {
+        std::wcerr << L"Ошибка запуска процесса 1\n";
+        return 1;
+    }
+
+    // Второй процесс
+    if (CreateProcessW(
+        childPath.c_str(), nullptr, nullptr, nullptr, FALSE,
+        CREATE_NEW_CONSOLE, nullptr, nullptr, &si2, &pi2)) {
+
+        SetProcessAffinityMask(pi2.hProcess, affinityMask);
+        SetPriorityClass(pi2.hProcess, priority2);
+        std::wcout << L"Процесс 2 запущен: PID = " << pi2.dwProcessId << "\n";
+    }
+    else {
+        std::wcerr << L"Ошибка запуска процесса 2\n";
+        return 1;
+    }
+
+
+    // Ожидание завершения дочерних процессов
+    WaitForSingleObject(pi1.hProcess, INFINITE);
+    WaitForSingleObject(pi2.hProcess, INFINITE);
+
+    // Очистка
+    CloseHandle(pi1.hProcess);
+    CloseHandle(pi1.hThread);
+    CloseHandle(pi2.hProcess);
+    CloseHandle(pi2.hThread);
+
+    std::wcout << L"Оба дочерних процесса завершены.\n";
+    return 0;
+}
---
+++ lab_5/Lab_5/Lab-05c/Lab-05c.cpp
@@ -0,0 +1,63 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <thread>
+#include <string>
+
+volatile LONG counter1 = 0;
+volatile LONG counter2 = 0;
+const int maxIterations = 1'000'000;
+
+DWORD WINAPI ThreadFunction(LPVOID param) {
+    int threadId = reinterpret_cast<int>(param);
+    for (int i = 0; i < maxIterations; ++i) {
+        if (i % 1000 == 0) {
+            std::wcout << L"[Поток " << threadId << L"] Итерация: " << i << L"\n";
+        }
+        if (threadId == 1) InterlockedIncrement(&counter1);
+        else InterlockedIncrement(&counter2);
+    }
+    return 0;
+}
+
+int wmain(int argc, wchar_t* argv[]) {
+    setlocale(LC_ALL, "ru");
+    if (argc != 5) {
+        std::wcerr << L"Использование: Lab-05c <P1: affinity mask> <P2: process priority> <P3: thread1 priority> <P4: thread2 priority>\n";
+        return 1;
+    }
+
+    DWORD_PTR affinityMask = _wtoi64(argv[1]);
+    DWORD processPriority = _wtoi(argv[2]);
+    int threadPriority1 = _wtoi(argv[3]);
+    int threadPriority2 = _wtoi(argv[4]);
+
+    std::wcout << L"Маска процессоров (P1): " << affinityMask << "\n";
+    std::wcout << L"Приоритет процесса (P2): " << processPriority << "\n";
+    std::wcout << L"Приоритет потока 1 (P3): " << threadPriority1 << "\n";
+    std::wcout << L"Приоритет потока 2 (P4): " << threadPriority2 << "\n";
+
+    // Установка приоритета процесса и маски
+    SetPriorityClass(GetCurrentProcess(), processPriority);
+    SetProcessAffinityMask(GetCurrentProcess(), affinityMask);
+
+    // Создание потоков
+    HANDLE hThread1 = CreateThread(nullptr, 0, ThreadFunction, (LPVOID)1, 0, nullptr);
+    HANDLE hThread2 = CreateThread(nullptr, 0, ThreadFunction, (LPVOID)2, 0, nullptr);
+
+    SetThreadPriority(hThread1, threadPriority1);
+    SetThreadPriority(hThread2, threadPriority2);
+
+    std::wcout << L"Ожидание 15 секунд для анализа в Process Explorer...\n";
+    Sleep(15'000);
+
+    WaitForSingleObject(hThread1, INFINITE);
+    WaitForSingleObject(hThread2, INFINITE);
+
+    std::wcout << L"Поток 1 завершён. Итераций: " << counter1 << "\n";
+    std::wcout << L"Поток 2 завершён. Итераций: " << counter2 << "\n";
+
+    CloseHandle(hThread1);
+    CloseHandle(hThread2);
+
+    return 0;
+}
